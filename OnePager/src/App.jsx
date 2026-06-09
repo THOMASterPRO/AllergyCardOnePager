f2df23624a264f3b49c61411af72b3d87ee43af0
THOMASterPRO <thomas@bezemer.net>
2026-06-10 00:07:59 +0200

    feat: enhance header layout and add allergy screenshot

--- OnePager/src/App.jsx
+++ OnePager/src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
 import Beams from './bits/beams/Beams';
+import allergyScreenshot from './img/allergyScreenshot.PNG';
 
 
 function App() {
   return (
     <>
       <div className='header'>
-        <h1 className='title'>AllergyCard</h1>
         <Beams
           beamWidth={3}
           beamHeight={30}
@@ -17,9 +17,20 @@ function App() {
           scale={0.2}
           rotation={30}
         />
+        <div className='headerContent'>
+          <div className='titleBlock'>
+            <h1 className='title'>Allergy</h1>
+            <h1 className='title'>Card</h1>
+          </div>
+          <img
+            className='screenshot'
+            src={allergyScreenshot}
+            alt='AllergyCard screenshot'
+          />
+        </div>
       </div>
       <div className='second'>
-
+        
       </div>
     </>
   )
